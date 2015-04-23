b3f13a385c8122bfb5e9ac491b66cc132fe1dc15
manee92 <kmerandi25@gmail.com>
2015-04-23 11:29:33 -0700

    UI changed.

--- app/src/main/java/com/example/maneesha/rsvp/ActivityMain.java
+++ app/src/main/java/com/example/maneesha/rsvp/ActivityMain.java
@@ -1,9 +1,12 @@
 package com.example.maneesha.rsvp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class ActivityMain extends Activity {
@@ -12,6 +15,14 @@ public class ActivityMain extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityMain.this, ListViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/example/maneesha/rsvp/AsyncResponse.java
@@ -0,0 +1,5 @@
+package com.example.maneesha.rsvp;
+
+public interface AsyncResponse {
+    void processFinish(String output);
+}
