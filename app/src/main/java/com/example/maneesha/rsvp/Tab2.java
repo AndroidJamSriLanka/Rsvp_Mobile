680d71c14b88e6a5be07286df3ed134da79bb5a0
manee92 <kmerandi25@gmail.com>
2015-04-26 19:34:42 -0700

    UI changed.

--- app/src/main/java/com/example/maneesha/rsvp/Tab2.java
+++ app/src/main/java/com/example/maneesha/rsvp/Tab2.java
@@ -1,43 +1,24 @@
 package com.example.maneesha.rsvp;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TabHost;
+import com.example.maneesha.rsvp.R;
 
-
-public class ActivityMain extends Activity {
+public class Tab2 extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_activity_main);
-
-        TabHost tabHost = (TabHost)findViewById(R.id.tabHost);
-        tabHost.setup();
-
-        TabHost.TabSpec tabSpec = tabHost.newTabSpec("Upcoming Events");
-        tabSpec.setContent(R.id.tab1);
-        tabSpec.setIndicator("Upcoming Events");
-        tabHost.addTab(tabSpec);
-
-        tabSpec = tabHost.newTabSpec("Registered Events");
-        tabSpec.setContent(R.id.tab2);
-        tabSpec.setIndicator("Registered Events");
-        tabHost.addTab(tabSpec);
-
-
+        setContentView(R.layout.activity_tab2);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+        getMenuInflater().inflate(R.menu.tab2, menu);
         return true;
     }
 
